f6fae08b39720856ca2279775c00557cbadba107
Tomohisa Uchida <uchida@uddystyle.com>
2024-06-30 03:49:08 +0900

    added basic setup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,27 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Don't auto commenting new lines
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufEnter", { pattern = "*", command = "set fo-=c fo-=r fo-=o" })
+
+-- don't show number
+vim.api.nvim_create_autocmd("TermOpen", {
+  command = [[setlocal nonumber norelativenumber]],
+})
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,52 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/Declement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwords
+keymap.set("n", "dw", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+
+-- diagnostics
+keymap.set("n", "C-j", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+-- Move
+keymap.set("n", "<C-h>", "^", opts)
+keymap.set("n", "<C-l>", "$", opts)
+
+-- New lines
+keymap.set("n", "O", [[:lua vim.fn.append(vim.fn.line('.'), '')<CR>j]], opts)
+
+-- Fast saving
+keymap.set("n", "<Leader>w", ":write!<CR>", opts)
+keymap.set("n", "<Leader>q", ":q!<CR>", opts)
+
+-- Move Right
+keymap.set("i", "<C-f>", "<Right>", opts)
+
+-- Buffer line
+keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", opts)
+keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", opts)
+
+-- Delete buffer
+keymap.set("n", "<leader>bd", ":bd!<CR>", opts)
+
+-- Toggle neotree
+keymap.set("n", "<leader>n", ":Neotree toggle left<CR>", opts)
+
+-- Create terminal buffer
+keymap.set("n", "<leader>tt", ":terminal<CR>", opts)
