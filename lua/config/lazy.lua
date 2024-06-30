475a6e20b0c0344e85ab7fc99b7099d4a43458f0
Tomohisa Uchida <uchida@uddystyle.com>
2024-07-01 03:01:54 +0900

    change colorscheme catppuccin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,11 +28,10 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight" } },
+  -- install = { colorscheme = { "tokyonight" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
         -- "matchit",
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,39 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "storm",
-    terminal_colors = true,
-    -- transparent = true,
-    styles = {
-      -- sidebars = "transparent",
-      -- floats = "transparent",
+  -- "folke/tokyonight.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- opts = {
+  --   style = "storm",
+  --   terminal_colors = true,
+  -- transparent = true,
+  -- styles = {
+  -- sidebars = "transparent",
+  -- floats = "transparent",
+  -- },
+  -- },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      cmp = true,
+      gitsigns = true,
+      mason = true,
+      markdown = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      telescope = true,
+      treesitter = true,
     },
+    config = function()
+      vim.cmd([[colorscheme catppuccin]])
+    end,
   },
 }
