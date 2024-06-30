01ae347d6442518eff356e98c47b93a4b1df062f
Tomohisa Uchida <uchida@uddystyle.com>
2024-07-01 03:34:10 +0900

    cleanup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,36 +1,46 @@
 return {
-  -- "folke/tokyonight.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- opts = {
-  --   style = "storm",
-  --   terminal_colors = true,
-  -- transparent = true,
-  -- styles = {
-  -- sidebars = "transparent",
-  -- floats = "transparent",
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     style = "storm",
+  --     terminal_colors = true,
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
   -- },
-  -- },
+
   {
     "catppuccin/nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      cmp = true,
-      gitsigns = true,
-      mason = true,
-      markdown = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
+      transparent_background = false,
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        mason = true,
+        markdown = true,
+        notify = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
         },
+        telescope = true,
+        treesitter = true,
       },
-      telescope = true,
-      treesitter = true,
     },
     config = function()
       vim.cmd([[colorscheme catppuccin]])
