36e98506ab8b98e5e2144fff471ddd1e5734c406
Tomohisa Uchida <uchida@uddystyle.com>
2024-07-14 04:00:33 +0900

    disable git sign for lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -97,41 +97,41 @@ return {
           lualine_c = {
             -- stylua: ignore
             { LazyVim.lualine.pretty_path() },
-            {
-              function()
-                return require("noice").api.status.mode.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.mode.has()
-              end,
-              color = function()
-                return LazyVim.ui.fg("Constant")
-              end,
-            },
+            -- {
+            --   function()
+            --     return require("noice").api.status.mode.get()
+            --   end,
+            --   cond = function()
+            --     return package.loaded["noice"] and require("noice").api.status.mode.has()
+            --   end,
+            --   color = function()
+            --     return LazyVim.ui.fg("Constant")
+            --   end,
+            -- },
             -- stylua: ignore
-            {
-              require("lazy.status").updates,
-              cond = require("lazy.status").has_updates,
-              color = function() return LazyVim.ui.fg("Special") end,
-            },
-            {
-              "diff",
-              symbols = {
-                added = icons.git.added,
-                modified = icons.git.modified,
-                removed = icons.git.removed,
-              },
-              source = function()
-                local gitsigns = vim.b.gitsigns_status_dict
-                if gitsigns then
-                  return {
-                    added = gitsigns.added,
-                    modified = gitsigns.changed,
-                    removed = gitsigns.removed,
-                  }
-                end
-              end,
-            },
+            -- {
+            --   require("lazy.status").updates,
+            --   cond = require("lazy.status").has_updates,
+            --   color = function() return LazyVim.ui.fg("Special") end,
+            -- },
+            -- {
+            --   "diff",
+            --   symbols = {
+            --     added = icons.git.added,
+            --     modified = icons.git.modified,
+            --     removed = icons.git.removed,
+            --   },
+            --   source = function()
+            --     local gitsigns = vim.b.gitsigns_status_dict
+            --     if gitsigns then
+            --       return {
+            --         added = gitsigns.added,
+            --         modified = gitsigns.changed,
+            --         removed = gitsigns.removed,
+            --       }
+            --     end
+            --   end,
+            -- },
           },
           -- stylua: ignore
           lualine_x = {
