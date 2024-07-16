1cd0ed422dd46cf288aa2fd69dbe4dfdd395fb72
Tomohisa Uchida <uchida@uddystyle.com>
2024-07-17 06:32:05 +0900

    added dap

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,161 +1,69 @@
 return {
 
-  -- {
-  --   "mfussenegger/nvim-dap",
-  --
-  --   dependencies = {
-  --     "rcarriga/nvim-dap-ui",
-  --     "jbyuki/one-small-step-for-vimkind",
-  --     {
-  --       "theHamsta/nvim-dap-virtual-text",
-  --       opts = {},
-  --     },
-  --   },
-  --
-  --   config = function()
-  --     local dap = require("dap")
-  --     dap.adapters.lldb = {
-  --       type = "executable",
-  --       command = "/opt/homebrew/opt/llvm/bin/lldb",
-  --       name = "lldb",
-  --     }
-  --
-  --     if LazyVim.has("mason-nvim-dap.nvim") then
-  --       require("mason-nvim.dap").setup(LazyVim.opts("mason-nvim-dap.vim"))
-  --     end
-  --
-  --     for name, sign in pairs(LazyVim.config.icons.dap) do
-  --       sign = type(sign) == "table" and sign or { sign }
-  --       vim.fn.sign_define(
-  --         "Dap" .. name,
-  --         { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-  --       )
-  --     end
-  --   end,
-  --
-  --   keys = {
-  --     { "<leader>d", "", desc = "+debug", mode = { "n", "v" } },
-  --     {
-  --       "<leader>dB",
-  --       function()
-  --         require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-  --       end,
-  --       desc = "Breakpoint Condition",
-  --     },
-  --     {
-  --       "<leader>db",
-  --       function()
-  --         require("dap").toggle_breakpoint()
-  --       end,
-  --       desc = "Toggle Breakpoint",
-  --     },
-  --     {
-  --       "<leader>dc",
-  --       function()
-  --         require("dap").continue()
-  --       end,
-  --       desc = "Continue",
-  --     },
-  --     {
-  --       "<leader>da",
-  --       function()
-  --         require("dap").continue({ before = get_args })
-  --       end,
-  --       desc = "Run with Args",
-  --     },
-  --     {
-  --       "<leader>dC",
-  --       function()
-  --         require("dap").run_to_cursor()
-  --       end,
-  --       desc = "Run to Cursor",
-  --     },
-  --     {
-  --       "<leader>dg",
-  --       function()
-  --         require("dap").goto_()
-  --       end,
-  --       desc = "Go to Line (No Execute)",
-  --     },
-  --     {
-  --       "<leader>di",
-  --       function()
-  --         require("dap").step_into()
-  --       end,
-  --       desc = "Step Into",
-  --     },
-  --     {
-  --       "<leader>dj",
-  --       function()
-  --         require("dap").down()
-  --       end,
-  --       desc = "Down",
-  --     },
-  --     {
-  --       "<leader>dk",
-  --       function()
-  --         require("dap").up()
-  --       end,
-  --       desc = "Up",
-  --     },
-  --     {
-  --       "<leader>dl",
-  --       function()
-  --         require("dap").run_last()
-  --       end,
-  --       desc = "Run Last",
-  --     },
-  --     {
-  --       "<leader>do",
-  --       function()
-  --         require("dap").step_out()
-  --       end,
-  --       desc = "Step Out",
-  --     },
-  --     {
-  --       "<leader>dO",
-  --       function()
-  --         require("dap").step_over()
-  --       end,
-  --       desc = "Step Over",
-  --     },
-  --     {
-  --       "<leader>dp",
-  --       function()
-  --         require("dap").pause()
-  --       end,
-  --       desc = "Pause",
-  --     },
-  --     {
-  --       "<leader>dr",
-  --       function()
-  --         require("dap").repl.toggle()
-  --       end,
-  --       desc = "Toggle REPL",
-  --     },
-  --     {
-  --       "<leader>ds",
-  --       function()
-  --         require("dap").session()
-  --       end,
-  --       desc = "Session",
-  --     },
-  --     {
-  --       "<leader>dt",
-  --       function()
-  --         require("dap").terminate()
-  --       end,
-  --       desc = "Terminate",
-  --     },
-  --     {
-  --       "<leader>dw",
-  --       function()
-  --         require("dap.ui.widgets").hover()
-  --       end,
-  --       desc = "Widgets",
-  --     },
-  --   },
-  -- },
+  {
+    "mfussenegger/nvim-dap",
+
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      require("dapui").setup()
+      require("nvim-dap-virtual-text").setup({
+        enabled = true,
+        enabled_commands = true,
+        highlight_changed_variables = true,
+        highlight_new_as_changed = false,
+      })
+      dap.adapters.lldb = {
+        type = "executable",
+        command = "/opt/homebrew/opt/llvm/bin/lldb",
+        name = "lldb",
+      }
+
+      for name, sign in pairs(LazyVim.config.icons.dap) do
+        sign = type(sign) == "table" and sign or { sign }
+        vim.fn.sign_define(
+          "Dap" .. name,
+          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        )
+      end
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F12>", dap.restart)
+
+      -- dap.listeners.before.attach["dapui_config"] = function()
+      --   dapui.open({})
+      -- end
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
+  },
 
   {
     "mrcjkb/rustaceanvim",
