4cbefc9bcfc1e6dbbaadb3e2c33f1d00cf43cb80
Tomohisa Uchida <uchida@uddystyle.com>
2024-08-16 14:42:07 +0900

    delete core.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "catppuccin-mocha",
+    } },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
