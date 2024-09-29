604f567e2e797d50076c60a0b391184f2126c647
Tomohisa Uchida <uchida@uddystyle.com>
2024-09-29 19:44:13 +0900

    display git diff for lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -120,24 +120,24 @@ return {
             --   cond = require("lazy.status").has_updates,
             --   color = function() return LazyVim.ui.fg("Special") end,
             -- },
-            -- {
-            --   "diff",
-            --   symbols = {
-            --     added = icons.git.added,
-            --     modified = icons.git.modified,
-            --     removed = icons.git.removed,
-            --   },
-            --   source = function()
-            --     local gitsigns = vim.b.gitsigns_status_dict
-            --     if gitsigns then
-            --       return {
-            --         added = gitsigns.added,
-            --         modified = gitsigns.changed,
-            --         removed = gitsigns.removed,
-            --       }
-            --     end
-            --   end,
-            -- },
+            {
+              "diff",
+              symbols = {
+                added = icons.git.added,
+                modified = icons.git.modified,
+                removed = icons.git.removed,
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
           },
           -- stylua: ignore
           lualine_x = {
