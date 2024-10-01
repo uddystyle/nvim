3a5eb1e6ca51f4a57f87e03480205ab2201b0ff9
Tomohisa Uchida <uchida@uddystyle.com>
2024-10-01 21:03:59 +0900

    change section for lialine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -102,7 +102,8 @@ return {
           lualine_b = { "branch" },
           lualine_c = {
             -- stylua: ignore
-            { LazyVim.lualine.pretty_path(), separator = "|" },
+            -- { LazyVim.lualine.pretty_path(), separator = "|" },
+            { LazyVim.lualine.pretty_path() },
             -- {
             --   function()
             --     return require("noice").api.status.mode.get()
@@ -120,6 +121,45 @@ return {
             --   cond = require("lazy.status").has_updates,
             --   color = function() return LazyVim.ui.fg("Special") end,
             -- },
+            -- {
+            --   "diff",
+            --   symbols = {
+            --     added = icons.git.added,
+            --     modified = icons.git.modified,
+            --     removed = icons.git.removed,
+            --   },
+            --   source = function()
+            --     local gitsigns = vim.b.gitsigns_status_dict
+            --     if gitsigns then
+            --       return {
+            --         added = gitsigns.added,
+            --         modified = gitsigns.changed,
+            --         removed = gitsigns.removed,
+            --       }
+            --     end
+            --   end,
+            -- separator = "|",
+            -- },
+            -- {
+            --   "diagnostics",
+            --   symbols = {
+            --     error = icons.diagnostics.Error,
+            --     warn = icons.diagnostics.Warn,
+            --     info = icons.diagnostics.Info,
+            --     hint = icons.diagnostics.Hint,
+            --   },
+            -- separator = "|",
+            -- },
+          },
+          -- stylua: ignore
+          lualine_x = {
+            LazyVim.lualine.root_dir(),
+            -- { "encoding", separator = "", padding = { left = 1, right = 1 } },
+            { "encoding", separator = "|", padding = { left = 1, right = 1 } },
+            -- { "fileformat", separator = "", padding = { left = 1, right = 1 } },
+            { "fileformat", separator = "|", padding = { left = 1, right = 1 } },
+            -- { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 1 } },
+            { "filetype", icon_only = false, separator = "|", padding = { left = 1, right = 1 } },
             {
               "diff",
               symbols = {
@@ -137,7 +177,7 @@ return {
                   }
                 end
               end,
-              separator = "|",
+            separator = "",
             },
             {
               "diagnostics",
@@ -147,17 +187,7 @@ return {
                 info = icons.diagnostics.Info,
                 hint = icons.diagnostics.Hint,
               },
-              separator = "|",
             },
-          },
-          -- stylua: ignore
-          lualine_x = {
-            LazyVim.lualine.root_dir(),
-            -- { "encoding", separator = "", padding = { left = 1, right = 1 } },
-            { "encoding", separator = "|", padding = { left = 1, right = 1 } },
-            -- { "fileformat", separator = "", padding = { left = 1, right = 1 } },
-            { "fileformat", separator = "|", padding = { left = 1, right = 1 } },
-            { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 1 } },
           },
           lualine_y = {
             { "progress", separator = " ", padding = { left = 1, right = 1 } },
