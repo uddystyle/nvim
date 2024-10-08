d0c73556a81569c043bdbc4eefc9622e4296817c
Tomohisa Uchida <uchida@uddystyle.com>
2024-10-08 22:56:29 +0900

    added colors for popup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -51,6 +51,7 @@ return {
       keywordStyle = { italic = false },
       statementStyle = { bold = true },
       colors = {
+        palette = {},
         theme = {
           all = {
             ui = {
@@ -75,6 +76,12 @@ return {
           -- set their background accordingly if you wish to keep them dark and borderless
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          -- For Popup
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
         }
       end,
     },
