1129fd5bff186dc296d117129d999762a2f9bc65
Tomohisa Uchida <uchida@uddystyle.com>
2026-09-06 09:21:47 +0900

    Enable relative line numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,7 @@ vim.cmd("filetype plugin on")
 vim.o.guicursor = ""
 vim.o.cursorline = false
 vim.o.number = true
-vim.o.relativenumber = false
+vim.o.relativenumber = true
 vim.o.laststatus = 0
 
 -- disable providers
